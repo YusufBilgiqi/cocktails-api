23c2f25ae0a04ceb0ea9168691eea114ca9e8167
YusufBilgiqi <debruyckersteven@gmail.com>
2023-04-28 15:52:51 +0100

    practice

--- practice/drink.js
+++ practice/drink.js
@@ -2,6 +2,10 @@
 
 const drinkCenter = document.querySelector(".drink-center");
 const singleCocktail = document.querySelector(".single-cocktail");
+const drinkImg = document.querySelector(".drink-img");
+const drinkTitle = document.querySelector(".drink-info");
+const drinkPrep = document.querySelector(".drink-prep");
+const ingredients = documument.querySelector(".ingredients");
 
 // eventlisteners
 
