92adfb9ff56f510c4dc42e225f66b8134e0b32bc
YusufBilgiqi <debruyckersteven@gmail.com>
2023-05-01 21:42:48 +0200

    localstorage

--- practice/app.js
+++ practice/app.js
@@ -1,56 +1,117 @@
 // selectors
 
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a";
-const singleCocktail = document.querySelector(".single-cocktail");
-const cocktailImg = document.querySelectorAll(".cocktail-img");
-const cocktailName = document.querySelectorAll(".cocktail-name");
-const cocktailSection = document.querySelector(".cocktail-section");
+// const singleCocktail = document.querySelector(".single-cocktail");
+// const cocktailImg = document.querySelectorAll(".cocktail-img");
+// const cocktailName = document.querySelectorAll(".cocktail-name");
 const form = document.querySelector(".input-form");
 const searchInput = document.getElementById("searchInput");
 
 // // eventListeners
-window.addEventListener("DOMContentLoaded", start);
 
-async function fetchCocktails() {
+// async function fetchCocktails() {
+//   try {
+//     const resp = await fetch(url);
+//     const data = await resp.json();
+
+//     return data.drinks;
+//   } catch (error) {
+//     cocktailSection.innerHTML = `We couldn't fetch the data, please fix the error.`;
+//   }
+// }
+
+// async function displayCocktails(cocktail) {
+//   let cocktailsElements = await cocktail
+//     .map((drink) => {
+//       const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
+//       console.log(id);
+//       return `<article class="single-cocktail" data-id="${id}" >
+//        <a href="">
+//        <img class="cocktail-img" src="${image}" alt="cocktail" />
+//      </a>
+//      <h2 class="cocktail-name">${name} </h2>
+//     </article>`;
+//     })
+//     .join("");
+//   cocktailSection.innerHTML = cocktailsElements;
+// }
+
+// const setDrink = (section) => {
+//   section.addEventListener("click", function (e) {
+//     // prevent navigation to new page
+//     e.preventDefault();
+//     let article = e.target.closest("article");
+//     const id = article.dataset.id;
+//     // console.log(id);
+
+//     // Set id to localstorage
+//     localStorage.setItem("drink", id);
+//     window.location.href = "./drink.html";
+//   });
+// };
+
+// window.addEventListener("DOMContentLoaded", start);
+
+// async function start() {
+//   const data = await fetchCocktails();
+//   const section = await displayCocktails(data);
+//   if (section) {
+//     setDrink(section);
+//   }
+// }
+
+const fetchDrinks = async () => {
   try {
     const resp = await fetch(url);
     const data = await resp.json();
-
     return data.drinks;
   } catch (error) {
-    cocktailSection.innerHTML = `We couldn't fetch the data, please fix the error.`;
+    return `We are not able to fetch the data"`;
   }
-}
+};
+fetchDrinks();
 
-async function displayCocktails(cocktail) {
-  let cocktailsElements = await cocktail.map((drink) => {
-    const { strDrink: id, strDrinkThumb: image } = drink;
-    return `<article class="single-cocktail">
-       <a href="">
-       <img class="cocktail-img" src="${image}" alt="cocktail" />
-     </a>
-     <h2 class="cocktail-name">${id} </h2>
-    </article>`;
-  });
-  cocktailSection.innerHTML = cocktailsElements.join("");
+const displayCocktails = async (data) => {
+  const section = document.querySelector(".cocktail-section");
 
-  // filter function
-  // filter function
-  form.addEventListener("keyup", () => {
-    const inputValue = searchInput.value.toLowerCase();
-    const filteredProducts = cocktailsElements.filter((product) => {
-      const title = product
-        .match(/<h2 class="cocktail-name">(.+)<\/h2>/i)[1]
-        .toLowerCase();
-      return title.includes(inputValue);
-    });
-    cocktailSection.innerHTML = filteredProducts.join("");
-  });
-}
+  if (!data) {
+    section.innerHTML = null;
+  }
+  let drink = data
+    .map((item) => {
+      const { idDrink: id, strDrink: name, strDrinkThumb: image } = item;
 
-async function start() {
-  const data = await fetchCocktails();
-  displayCocktails(data);
-  //   searchCocktailsByName(data);
-}
+      return `<article class="single-cocktail" data-id=${id}>
+    <a href="">
+    <img class="cocktail-img" src="${image} " alt="cocktail" />
+  </a>
+  <h2 class="cocktail-name">${name} </h2>
+</div>
+</article>`;
+    })
+    .join("");
+
+  section.innerHTML = drink;
+  return section;
+};
+
+const start = async () => {
+  const data = await fetchDrinks();
+  const section = await displayCocktails(data);
+  console.log(section);
+  if (section) {
+    storage(section);
+  }
+};
 start();
+
+const storage = (section) => {
+  section.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let article = e.target.closest("article");
+    const id = article.dataset.id;
+    localStorage.setItem("drink", id);
+    window.location.href = "./drink.html";
+  });
+};
--- practice/drink.js
+++ practice/drink.js
@@ -1,5 +1,6 @@
 // selectors
 
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a";
 const drinkCenter = document.querySelector(".drink-center");
 const drinkImg = document.querySelector(".drink-img");
 const drinkTitle = document.querySelector(".drink-info");
@@ -10,14 +11,59 @@ const ingredients = document.querySelector(".ingredients");
 
 // functions
 
-// async function fetchData() {
-//   try {
-//   } catch (error) {
-//     drinkCenter.innerHTML = `We couldn't fetch the requested data`;
-//   }
-// }
+async function fetchCocktails(url) {
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
 
-// function displayCocktail() {
-//   fetchCocktails();
-//   console.log(fetchCocktails());
-// }
+    return data.drinks;
+  } catch (error) {
+    cocktailSection.innerHTML = `We couldn't fetch the data, please fix the error.`;
+  }
+}
+
+const displayDrink = (data) => {
+  const drink = data[0];
+  //   console.log(drink);
+  const { strDrinkThumb: image, strDrink: name, strInstructions: desc } = drink;
+  //   console.log(name, desc);
+  const list = [
+    drink.strIngredient1,
+    drink.strIngredient2,
+    drink.strIngredient3,
+    drink.strIngredient4,
+    drink.strIngredient5,
+  ];
+  const img = document.querySelector(".drink-img");
+  const drinkTitle = document.querySelector(".drink-title");
+  const drinkPrep = document.querySelector(".drink-prep");
+  const ingredients = document.querySelector(".ingredients");
+  img.src = image;
+  document.title = name;
+  drinkPrep.textContent = desc;
+  drinkTitle.textContent = name;
+  ingredients.innerHTML = list
+    .map((item) => {
+      if (!item) {
+        return;
+      } else {
+        return `<li><i class="fa-solid fa-square-check"></i> ${item}</li>`;
+      }
+    })
+    .join("");
+  return ``;
+};
+
+const presentDrink = async () => {
+  const id = localStorage.getItem("drink");
+  if (!id) {
+    window.location.replace = "./index.html";
+  } else {
+    const drink = await fetchCocktails(
+      `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+    );
+    displayDrink(drink);
+  }
+};
+
+window.addEventListener("DOMContentLoaded", presentDrink);
